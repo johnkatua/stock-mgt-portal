b62dc557030b997c0b44aa580fda4da50c6c819c
johnkatua <johnkatua99@gmail.com>
2024-08-22 16:50:23 +0300

    add missing packages

--- app/components/login/LoginForm.tsx
+++ app/components/login/LoginForm.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { FC } from 'react';
+import CustomForm from '../shared/CustomForm';
+import CustomFormItem from '../shared/CustomFormItem';
+import CustomInput from '../shared/CustomInput';
 
-const LoginForm = () => {
-  return <div>LoginForm</div>;
+type HandleSubmitType = (_values: { email: string; password: string }) => void;
+
+interface LoginFormProps {
+  handleSubmit: HandleSubmitType;
+}
+
+const LoginForm: FC<LoginFormProps> = ({ handleSubmit }) => {
+  return (
+    <CustomForm onFinish={handleSubmit}>
+      <CustomFormItem
+        name='email'
+        label='Email Address'
+        rules={[
+          {
+            required: true,
+            message: 'Please enter your email address',
+          },
+        ]}
+      >
+        <CustomInput placeholder='Email Address' />
+      </CustomFormItem>
+      <CustomFormItem
+        name='password'
+        label='Password'
+        rules={[
+          {
+            required: true,
+            message: 'Please enter your password',
+          },
+        ]}
+      >
+        <CustomInput placeholder='Password' type='password' />
+      </CustomFormItem>
+    </CustomForm>
+  );
 };
 
 export default LoginForm;
--- app/components/login/LoginPage.tsx
+++ app/components/login/LoginPage.tsx
@@ -1,11 +1,15 @@
 import styles from '../../page.module.css';
+import LoginForm from './LoginForm';
 
 const LoginPage = () => {
+  const handleSubmit = (values: any) => {
+    console.log(values);
+  };
   return (
     <main className={styles.loginPage}>
       <div className={styles.loginWrapper}>
         <h1>Lakehouse</h1>
-        <div>Login Form</div>
+        <LoginForm handleSubmit={handleSubmit} />
       </div>
     </main>
   );
