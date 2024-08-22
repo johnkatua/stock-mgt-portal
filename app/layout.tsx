2296e0f9e14edfbeea33fb03ba2cb60864cf9632
johnkatua <johnkatua99@gmail.com>
2024-08-22 13:07:40 +0300

    add antd to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/** Wrap children with AntdRegistry to avoid page flicker */}
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
