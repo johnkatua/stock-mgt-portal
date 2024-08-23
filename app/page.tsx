b90ab1fa0202ed69c9496c624d5f8a88f5a833a9
johnkatua <johnkatua99@gmail.com>
2024-08-23 17:57:50 +0300

    add redux for state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Provider } from 'react-redux';
 import LoginContainer from './components/login';
+import { store } from './store/store';
 
 export default function Home() {
-  return <LoginContainer />;
+  return (
+    <Provider store={store}>
+      <LoginContainer />
+    </Provider>
+  );
 }
