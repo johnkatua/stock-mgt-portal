c12f1f0149148d48c6c254aed66e3d4757c193bc
johnkatua <johnkatua99@gmail.com>
2024-08-25 23:57:42 +0300

    test user logs in successfully

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,11 @@
 describe('Login', () => {
   it('Navigate to the application', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
+
+    cy.get('[data-testid=email-input]').type('test.user@example.com');
+
+    cy.get('[data-testid=password-input]').type('');
+
+    cy.get('[data-testid=login-btn]').click();
   });
 });
