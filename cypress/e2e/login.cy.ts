764cc5f3ccac1defa07e6d597fb81344b32e5a55
johnkatua <johnkatua99@gmail.com>
2024-09-23 21:38:17 +0300

    fix: add cypress secrets

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,6 +1,8 @@
 describe('Login', () => {
   it('Navigate to the application', () => {
-    const { email, password } = Cypress.env();
+    // const { email, password } = Cypress.env();
+    const email = Cypress.env('USER_EMAIL');
+    const password = Cypress.env('USER_PASSWORD');
 
     cy.login(email, password);
 
